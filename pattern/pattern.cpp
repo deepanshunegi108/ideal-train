e1cb4de604ac74d936a99c9ac17a7e57d8445bd1
Deepanshu Negi <152290049+deepanshunegi108@users.noreply.github.com>
2024-06-30 19:21:14 +0530

    Update pattern.cpp

--- pattern/pattern.cpp
+++ pattern/pattern.cpp
@@ -1,2 +1,26 @@
+// how loops works 
+// this code will print the pattern 
+//usage of function (void)
+// *****
+// *****
+// *****
+// *****
+// *****
 #include <iostream>
 using namespace std;
+
+ void pattern(int n){
+    for (int i=0;i<n; i++){
+        for (int j=0; j<n; j++){
+            cout << "*";
+        }
+        cout << endl;
+
+    }
+}
+int main(){
+int n;
+cin >> n;
+pattern(n);
+return 0;
+}
